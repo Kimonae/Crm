07befa6342aa40fe6c31257f4417873eb8d17e12
Kimonae <shinokasu@gmail.com>
2022-09-16 13:53:40 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageSignInComponent } from './login/page/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/page/page-sign-up/page-sign-up.component';
+import { PageResetPasswordComponent } from './login/page/page-reset-password/page-reset-password.component';
+import { PageForgotPasswordComponent } from './login/page/page-forgot-password/page-forgot-password.component';
 
+
+//routing
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },//redirection sur sign-in, full = complète restriction
   { path: 'sign-in', component: PageSignInComponent },//va chercher le composant
+  { path: 'sign-up', component: PageSignUpComponent},
+  { path: 'reset-password', component: PageResetPasswordComponent},
+  { path: 'forgot-password', component: PageForgotPasswordComponent}
+
 ];
 
 @NgModule({
