29224f38e3127f89d65e7bad8467d6e841660884
Kimonae <shinokasu@gmail.com>
2022-09-16 13:33:11 +0200

    add pages, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageSignInComponent } from './login/page/page-sign-in/page-sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },//redirection sur sign-in, full = complète restriction
+  { path: 'sign-in', component: PageSignInComponent },//va chercher le composant
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
