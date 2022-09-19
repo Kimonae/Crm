1237a059c17c2acaf5bbab742287fedd5fe4ad10
Kimonae <shinokasu@gmail.com>
2022-09-19 13:35:07 +0200

    json server, routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { TemplatesModule } from '../templates/templates.module';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -15,7 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
     NavComponent,
     FooterComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule,
+  RouterModule],
   exports: [UiModule, IconsModule, TemplatesModule, HeaderComponent, NavComponent, FooterComponent]
 
 })
