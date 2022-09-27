19d29655013dcf080a2258133a858b138a47af1d
Kimonae <shinokasu@gmail.com>
2022-09-27 12:00:07 +0200

    add

--- src/app/orders/page/page-add-order/page-add-order.component.ts
+++ src/app/orders/page/page-add-order/page-add-order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Order } from 'src/app/core/models/order';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-page-add-order',
@@ -14,7 +16,11 @@ export class PageAddOrderComponent implements OnInit {
 
   public item = new Order();
 
-  constructor() { }
+  constructor(
+
+    private ordersService : OrdersService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -22,5 +28,12 @@ export class PageAddOrderComponent implements OnInit {
   onAdd(obj: Order) {
 
 console.log(obj, "onAdd()")
+
+this.ordersService.add(obj).subscribe(()=>{
+
+this.router.navigate(['orders']);
+
+})
+
   }
 }
